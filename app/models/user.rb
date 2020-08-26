66b4d89db3acfc3be14e6a0930c92379fbebfe5a
adilsarwar@logicon.com.pk <adilsarwar@logicon.com.pk>
2020-08-27 00:37:25 +0500

    Setup Database, Configurations and Routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   mount_uploader :avatar, AvatarUploader
   validates :email, presence: true, uniqueness: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
   validates :username, presence: true, uniqueness: true
   validates :password,
-            length: { minimum: 6 },
+            length: {minimum: 6},
             if: -> { new_record? || !password.nil? }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  namespace :dashboard, defaults: {format: :json} do
-    namespace :api do
+    namespace :api, defaults: {format: :json} do
       namespace :v1 do
-        resources :users
-        post '/auth/login', to: 'authentication#login'
       end
     end
-  end
   get 'welcome/index'
 end
